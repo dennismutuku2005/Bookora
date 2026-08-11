ec81c177bb2e0985cd6baeead8f85d62bab59ba1
dennismuuo-dev <dennismutuku.dev@gmail.com>
2026-08-11 20:59:50 +0300

    update: refined build.gradle.kts functionality

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,14 +5,6 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.hilt) apply false
+    id("com.google.gms.google-services") version "4.5.0" apply false
 }
 
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.google.gms:google-services:4.3.15")
-    }
-}
