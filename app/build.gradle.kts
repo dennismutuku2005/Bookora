0940534e148c7827acff641ea432adf91211d909
dennismuuo-dev <dennismutuku.dev@gmail.com>
2026-08-11 16:31:41 +0300

    update: refined build.gradle.kts functionality

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation("androidx.compose.material:material-icons-extended")
 
     // Lifecycle
     implementation(libs.androidx.lifecycle.viewmodel.compose)
