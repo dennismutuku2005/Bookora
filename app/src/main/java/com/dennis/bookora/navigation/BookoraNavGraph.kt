d12b308069d11d7efd762523aff580cc2368e965
dennismuuo-dev <dennismutuku.dev@gmail.com>
2026-08-11 22:23:33 +0300

    update: refined BookoraNavGraph.kt functionality

--- app/src/main/java/com/dennis/bookora/navigation/BookoraNavGraph.kt
+++ app/src/main/java/com/dennis/bookora/navigation/BookoraNavGraph.kt
@@ -3,6 +3,9 @@ package com.dennis.bookora.navigation
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.LaunchedEffect
+import com.dennis.bookora.repository.auth.FirebaseAuthManager
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
@@ -18,9 +21,14 @@ fun BookoraNavGraph(
     navController: NavHostController = rememberNavController(),
     modifier: Modifier = Modifier
 ) {
+    val context = LocalContext.current
+    // Ensure Firebase is initialized and choose start destination based on current user
+    FirebaseAuthManager.ensureInitialized(context)
+    val startDestination = if (FirebaseAuthManager.currentUser() != null) BookoraDestinations.Main else BookoraDestinations.Welcome
+
     NavHost(
         navController = navController,
-        startDestination = BookoraDestinations.Welcome,
+        startDestination = startDestination,
         modifier = modifier
     ) {
         composable(BookoraDestinations.Welcome) {
