136af70e4f3746fef1532233784b04e8c4dd5fa9
dennismuuo-dev <dennismutuku.dev@gmail.com>
2026-08-11 20:58:48 +0300

    update: refined BookoraApplication.kt functionality

--- app/src/main/java/com/dennis/bookora/BookoraApplication.kt
+++ app/src/main/java/com/dennis/bookora/BookoraApplication.kt
@@ -1,7 +1,13 @@
 package com.dennis.bookora
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class BookoraApplication : Application()
+class BookoraApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseApp.initializeApp(this)
+    }
+}
