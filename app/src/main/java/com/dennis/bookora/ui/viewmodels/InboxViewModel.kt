08d17edd98d8524b4ed44f959cc30a38b822134e
dennismuuo-dev <dennismutuku.dev@gmail.com>
2026-08-13 17:01:32 +0300

    feature: added InboxViewModel.kt

---
+++ app/src/main/java/com/dennis/bookora/ui/viewmodels/InboxViewModel.kt
@@ -0,0 +1,43 @@
+package com.dennis.bookora.ui.viewmodels
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dennis.bookora.models.ChatConversation
+import com.dennis.bookora.models.Notification
+import com.dennis.bookora.repository.BookRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class InboxViewModel @Inject constructor(
+    private val repo: BookRepository
+) : ViewModel() {
+
+    var conversations = mutableStateOf<List<ChatConversation>>(emptyList())
+        private set
+
+    var notifications = mutableStateOf<List<Notification>>(emptyList())
+        private set
+
+    var isLoading = mutableStateOf(false)
+        private set
+
+    init {
+        loadData()
+    }
+
+    fun loadData() {
+        viewModelScope.launch {
+            isLoading.value = true
+            try {
+                conversations.value = repo.getConversations()
+                notifications.value = repo.getNotifications()
+            } catch (_: Exception) {
+            } finally {
+                isLoading.value = false
+            }
+        }
+    }
+}
