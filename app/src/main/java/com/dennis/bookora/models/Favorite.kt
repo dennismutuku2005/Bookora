8d2b94bb035b94e4432938b5085e10d701ecd8b7
dennismuuo-dev <dennismutuku.dev@gmail.com>
2026-08-13 16:58:56 +0300

    update: refined Favorite.kt functionality

--- app/src/main/java/com/dennis/bookora/models/Favorite.kt
+++ app/src/main/java/com/dennis/bookora/models/Favorite.kt
@@ -1,7 +1,8 @@
 package com.dennis.bookora.models
 
 data class Favorite(
-    val id: String,
-    val userId: String,
-    val bookId: String
+    val id: String = "",
+    val userId: String = "",
+    val bookId: String = "",
+    val timestamp: Long = 0L
 )
