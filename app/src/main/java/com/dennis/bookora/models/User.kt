a18fc3fec200224cbd94bc9ab9d8dd82707ee96d
dennismuuo-dev <dennismutuku.dev@gmail.com>
2026-08-13 14:08:48 +0300

    update: refined User.kt functionality

--- app/src/main/java/com/dennis/bookora/models/User.kt
+++ app/src/main/java/com/dennis/bookora/models/User.kt
@@ -4,6 +4,7 @@ data class User(
     val id: String,
     val firstName: String,
     val lastName: String,
+    val username: String,
     val email: String,
     val phone: String,
     val avatarUrl: String,
